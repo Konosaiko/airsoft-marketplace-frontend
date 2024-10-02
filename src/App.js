74f30f08161a5388c35e64f287d0172f6a7db30f
Konosaiko <xkonotv@gmail.com>
2024-10-02 15:27:11 +0200

    Configuration des routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/listing/:id" component={ListingPage} />
+        <Route path="/create-listing" component={CreateListingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+        <Route path="/profile" component={UserProfilePage} />
+        <Route path="/search" component={SearchResultsPage} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
