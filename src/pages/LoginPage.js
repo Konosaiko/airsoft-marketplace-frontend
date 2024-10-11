b02f081b397b0304529b35d7ac0a52ce8a00c008
Konosaiko <xkonotv@gmail.com>
2024-10-11 12:42:48 +0200

    Ajotu de l'affichage de la page Login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import LoginForm from '../components/LoginForm';
+import { Link } from 'react-router-dom';
 
-function LoginPage() {
-  return <div>Login Page</div>;
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Connexion à votre compte
+        </h2>
+      </div>
+
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <LoginForm />
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+                <span className="px-2 bg-white text-gray-500">
+                  Vous n'avez pas de compte?
+                </span>
+              </div>
+            </div>
+            <div className="mt-6">
+              <Link
+                to="/register"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+              >
+                S'inscrire
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default LoginPage;
