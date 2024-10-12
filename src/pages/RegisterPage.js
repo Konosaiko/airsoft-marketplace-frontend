4b1ec34c974bb662322bc5068dd3eb0e699a10fd
Konosaiko <xkonotv@gmail.com>
2024-10-12 17:13:08 +0200

    Modification affichage registerpage pour utiliser tailwind

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default function RegisterPage() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start pt-20 sm:pt-24">
+      <div className="w-full max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 mb-8">
           Créer un compte
         </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <RegisterForm />
           <div className="mt-6">
@@ -21,7 +18,7 @@ export default function RegisterPage() {
               </div>
               <div className="relative flex justify-center text-sm">
                 <span className="px-2 bg-white text-gray-500">
-                  Vous avez déjà un compte?
+                  Vous avez déjà un compte ?
                 </span>
               </div>
             </div>
