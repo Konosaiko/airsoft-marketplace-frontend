df3740375d57b25e4ee615f366c9700514485b9a
Konosaiko <xkonotv@gmail.com>
2024-10-12 17:13:16 +0200

    Modification affichage loginpage pour utiliser tailwind

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Connexion à votre compte
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start pt-20 sm:pt-24">
+      <div className="w-full max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 mb-8">
+          Connexion
         </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <LoginForm />
           <div className="mt-6">
